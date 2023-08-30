3ba40a37042ddbd216aea4e6f6ab2aac37a6f757
munene-m <munenenjue18@gmail.com>
2023-08-30 12:19:32 +0300

    console log server runnng line instead of using logger

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use("/api/v1/cart", cartRoute)
 
 // Start the server
 app.listen(PORT, () => {
-  // console.log(`Server running on port ${PORT}`);
-  logger.info(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
+  // logger.info(`Server running on port ${PORT}`)
 });
